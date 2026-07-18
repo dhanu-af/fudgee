89406620f39f95d76f92a46f525d2ded2a9c3bfe
Dhanu <dhanu-af@users.noreply.github.com>
2026-07-18 13:54:57 +1000

    Add bookkeeping SQL for mustChangePassword migration

---
+++ prisma/migration-bookkeeping-2.sql
@@ -0,0 +1,2 @@
+INSERT INTO "_prisma_migrations" (id, checksum, finished_at, migration_name, started_at, applied_steps_count)
+VALUES ('b6e10ac4-6c44-4713-8c3f-5ff3ba6c12e8', 'c4842d8c0a8eb6116965565190255308a7059a37c70b85dc17be256becc74d10', now(), '20260718034937_add_must_change_password', now(), 1);
